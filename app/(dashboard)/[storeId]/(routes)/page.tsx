977b110d41bcea0db1c7a8b1bc0d7386ad6bba1d
Satyajit444 <satyajitsenapati444@gmail.com>
2024-01-14 21:46:48 +0530

    api for store creation

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,12 @@
+const DashboardPage = () =>{
+    return(
+        <div>
+            this is a dashboard
+        </div>
+    )
+}
+
+export default DashboardPage;
+
+
+
--- app/api/store/route.ts
+++
@@ -1,9 +0,0 @@
-export async function POST(
-    req:Request,
-){
-    try{
-
-    }catch(error){
-
-    }
-}
