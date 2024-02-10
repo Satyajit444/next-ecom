1294879c29d34222e83cd48eebbb0db9aa89195b
Satyajit444 <satyajitsenapati444@gmail.com>
2024-02-11 01:11:39 +0530

    billboard added

---
+++ app/api/[storeId]/billboards/route.ts
@@ -0,0 +1,75 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!label) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    if (!params.storeId) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    if (!imageUrl) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const storeByUserid = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+    if (!storeByUserid) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
